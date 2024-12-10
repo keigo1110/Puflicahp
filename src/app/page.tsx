31a9131bdf8b13cb4fd10d9a6b73b513743bed6e
keigo1110 <mkeigo1110@gmail.com>
2024-12-10 19:15:16 +0900

    update YouTube link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,10 @@ export default function Home() {
       <MainContent
         title="Puflica"
         subtitle="ドスン，ビュン，ストン，プカプカ．"
-        videoId="YOUR_VIDEO_ID"
+        videoId="https://www.youtube.com/embed/RtaOnVdCqcU?si=hUaegbv18TR0Y0t7"
         concept={`膜によって実体を持った空気が様々な重力感をもって上下に揺れ動き，\n\n独立した振る舞いはあるとき突然揃いはじめる．\n\nそんな付いて離れていく不思議な様子を体感してほしい．`}
       />
       <Footer />
     </main>
   )
 }
-
